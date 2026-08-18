8adf600ca553f0a3f4d430e533714fa9088baa0a
Ryker <114112365+Ryker-p4x@users.noreply.github.com>
2026-08-18 15:56:02 -0300

    Add: dynamicArray - submission-0

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.cpp
@@ -0,0 +1,57 @@
+class DynamicArray {
+public:
+    int *arr;
+    int length;
+    int capacity;
+
+    DynamicArray(int capacity) {
+        this->capacity = capacity;
+        this->length = 0;
+        arr = new int[capacity];
+    }
+
+    int get(int i) {
+        return arr[i];
+    }
+
+    void set(int i, int n) {
+        arr[i] = n;
+    }
+
+    void pushback(int n) {
+        if (length == capacity) {
+            resize();
+        }
+
+        arr[length] = n;
+        length++;
+    }
+
+    int popback() {
+        int temp = arr[length - 1];
+        length--;
+        return temp;
+    }
+
+    void resize() {
+        capacity *= 2;
+
+        int *new_arr = new int[capacity];
+
+        for (int i = 0; i < length; i++) {
+            new_arr[i] = arr[i];
+        }
+
+        delete[] arr;
+
+        arr = new_arr;
+    }
+
+    int getSize() {
+        return length;
+    }
+
+    int getCapacity() {
+        return capacity;
+    }
+};
