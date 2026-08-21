0fc2c3f1ada719001eec5cf5adbdbaffcd8a1845
Ryker <114112365+Ryker-p4x@users.noreply.github.com>
2026-08-21 14:14:10 -0300

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.cpp
@@ -0,0 +1,26 @@
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> output(n);
+
+        output[0] = 1;
+        for (int i = 1; i < n; i++) {
+            output[i] = output[i - 1] * nums[i - 1];
+        }
+
+        int right_product = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            output[i] = output[i] * right_product;
+            right_product = right_product * nums[i];
+        }
+
+        return output;
+    }
+};
