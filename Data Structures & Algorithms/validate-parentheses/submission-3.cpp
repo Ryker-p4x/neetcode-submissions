f66d5984623fa2390b8dd72b275308ef1eb6a909
Ryker <114112365+Ryker-p4x@users.noreply.github.com>
2026-08-28 17:37:20 -0300

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,31 @@
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> charStack;
+
+        for (char c : s) {
+            if (c == '{' || c == '[' || c == '(') {
+                charStack.push(c);
+            } else {
+                if (charStack.empty()) return false;
+
+                char top = charStack.top();
+                if ((c == '}' && top == '{') ||
+                    (c == ')' && top == '(') ||
+                    (c == ']' && top == '[')) {
+                    charStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return charStack.empty();
+    }
+};
