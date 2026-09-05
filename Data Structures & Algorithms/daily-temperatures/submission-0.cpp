bf62e92b3e46f721737df5c65408302661d6904a
Ryker <114112365+Ryker-p4x@users.noreply.github.com>
2026-09-05 13:03:02 -0300

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,43 @@
+// Daily Temperatures
+// Medium
+// Topics
+// Company Tags
+// Hints
+// You are given an array of integers temperatures where temperatures[i] represents the daily temperatures on the ith day.
+// 
+// Return an array result where result[i] is the number of days after the ith day before a warmer temperature appears on a future day. If there is no day in the future where a warmer temperature will appear for the ith day, set result[i] to 0 instead.
+// 
+// Example 1:
+// 
+// Input: temperatures = [30,38,30,36,35,40,28]
+// 
+// Output: [1,4,1,2,1,0,0]
+// Example 2:
+// 
+// Input: temperatures = [22,21,20]
+// 
+// Output: [0,0,0]
+// Constraints:
+// 
+// 1 <= temperatures.length <= 100,000.
+// 1 <= temperatures[i] <= 100
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        std::stack<int> stack_;
+        vector<int> res(temperatures.size(), 0);
+        
+        for (int i = 0; i < temperatures.size(); i++) {
+            while (!stack_.empty() && temperatures[i] > temperatures[stack_.top()]) {
+                int prevIndex = stack_.top();
+                stack_.pop();
+                res[prevIndex] = i - prevIndex; // Record the wait time for the popped day
+            }
+            
+            stack_.push(i);
+        }
+
+        return res;
+    }
+};
